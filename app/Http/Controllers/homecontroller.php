88119ada662da51327200414b3b20cb82f69cc9b
unknown <sathasivam@hridhamtech.com>
2020-04-14 18:06:15 +0530

    home

--- app/Http/Controllers/homecontroller.php
+++ app/Http/Controllers/homecontroller.php
@@ -112,3 +112,82 @@ public function paginate($items, $perPage = 5, $page = null, $options = [])
 }
 
 
+class TestimonialsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function hometest()
+    {
+        try {
+
+            $client = new Client();
+            $response = $client->request(
+                'GET',
+                'http://restschool.hridham.com/api/getAllReview',
+                []
+            );
+
+            //  dd($response);
+            //  return false;
+
+            if ($response->getStatusCode() === 200) {
+                //  echo $response->getBody();
+
+                $testimonial_data = json_decode($response->getBody());
+                $data = $this->paginate($testimonial_data->data);
+dd($data);exit;
+                //  echo Session::get('access_token');
+                //  return false;
+                // return view('testimonial');
+                return view('index', ['testimonials' => $data]);
+            }
+        } catch (BadResponseException $ex) {
+            return dd($ex->getMessage());
+            $data = json_decode($ex->getResponse()->getBody()->getContents(), true);
+            $errors = [];
+            foreach ($data as $k => $v)
+                $errors[$k] = $v;
+            return redirect('index')->with('error', "Error! There is an error in album images loading");
+            //return view('auth.register')->with(['error'=>$errors]);
+        }
+        if ($response->getStatusCode() == 201) {
+            $albums = json_decode($response->getBody(), true);
+            //  return view('gallery')->with('albums',compact('albums'));
+
+        } else {
+            return 'Internal Server Error!<br>Check api/users/create<br>' . $response;
+        }
+    }
+    public function show($id)
+    {
+        $client = new Client();
+        $response = $client->request(
+            'GET',
+            'http://restschool.hridham.com/api/review/' . $id,
+            []
+        );
+
+        if ($response->getStatusCode() === 200) {
+            //  echo $response->getBody();
+
+            $testimonial_data = json_decode($response->getBody());
+            $data = $testimonial_data->data;
+
+            //  echo Session::get('access_token');
+            //  return false;
+            // return view('testimonial');
+            return view('index', ['testimonials' => $data]);
+        }
+    }
+    public function paginate($items, $perPage = 10, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        $paginatedItems = new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+        $paginatedItems->setPath(url('/testimonial'));
+        return $paginatedItems;
+    }
+}
