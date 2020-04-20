73e528b852f0bc578dbd896b1361e72b93b752c0
k2gsalem <k2gsalem@gmail.com>
2020-04-20 20:39:14 +0530

    sparkle kids

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,54 +1,7 @@
-
-   @extends('master')
+@extends('master')
 
    @section('content')
 
-
-   <section>
-    <div class=" heading-section ftco-animate">
-        <h2 class="mb-4 text-center ptitle text-primary" >Gallery</h2>
-
-      </div>
-    <div class="container">
-
-        <div class="portfolio-item row">
-
-           {{-- /*resize image using css code* --}}
-
-           {{-- style="object-fit:contain;width:100%;height:auto;"  --}}
-           @foreach($albums as $album)
-           {{-- @dd($albums) --}}
-           {{ app()->call('App\Http\Controllers\AlbumController@imageresize',['src'=>$album->cover_picture])}}
-        <a href="{{ url('albums/'.$album->id) }}" class="album">
-            <div class="item gts col-lg-3 col-md-3 col-6 col-sm" style="border:3px solid #1EAAF1; padding:0px; margin-top:15px;">
-
-                <div style="text-align:center;" class="bg-primary text-white p-2"><b>{{ $album->album_name }}</b></div>
-                <a href="http://restschool.hridham.com/storage/cover_pictures/{{ $album->cover_picture }}" class="fancylight" data-fancybox-group="light" style="margin-left:25px;">
-                <img class="img-fluid" src="{{ url('storage/cover_pictures/'.$album->cover_picture) }}" alt="">
-                </a>
-                {{-- <address> --}}
-              {{-- @if(!is_null($album->album_description)) --}}
-
-                    <p class="bg-primary text-white p-2">{{ $album->album_description }}</p>
-
-                {{-- </address> --}}
-                </div>
-            {{-- @endif --}}
-            </a>
-            @endforeach
-
-
-        </div>
-
-        <div class="row justify-content-center">
-            <div class="pagination">
-                {{ $albums->links() }}
-             </div>
-
-        </div>
-     </div>
-
-</section>
 
     <section class="bg-light">
         <div class=" heading-section ftco-animate">
@@ -110,74 +63,5 @@
          </div>
 
     </section>
-    <br><br>
-    @endsection
-    <!--<button class="btn btn-primary offset-5" type="button" ><a href="{{ url('/')}}" style="color:black">load more</a></button>
-    -->
-	<!--	<section class="ftco-section">
-    	<div class="container">
-    		<div class="row">
-        	<div class="col-md-6 col-lg-3 ftco-animate">
-        		<div class="pricing-entry bg-light pb-4 text-center">
-        			<div>
-	        			<h3 class="mb-3">Basic</h3>
-	        			<p><span class="price">$24.50</span> <span class="per">/ 5mos</span></p>
-	        		</div>
-	        		<div class="img" style="background-image: url(images/bg_1.jpg);"></div>
-	        		<div class="px-4">
-	        			<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-        			</div>
-        			<p class="button text-center"><a href="#" class="btn btn-primary px-4 py-3">Take A Course</a></p>
-        		</div>
-        	</div>
-        	<div class="col-md-6 col-lg-3 ftco-animate">
-        		<div class="pricing-entry bg-light pb-4 text-center">
-        			<div>
-	        			<h3 class="mb-3">Standard</h3>
-	        			<p><span class="price">$34.50</span> <span class="per">/ 5mos</span></p>
-	        		</div>
-	        		<div class="img" style="background-image: url(images/bg_2.jpg);"></div>
-        			<div class="px-4">
-	        			<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-        			</div>
-        			<p class="button text-center"><a href="#" class="btn btn-secondary px-4 py-3">Take A Course</a></p>
-        		</div>
-        	</div>
-        	<div class="col-md-6 col-lg-3 ftco-animate">
-        		<div class="pricing-entry bg-light active pb-4 text-center">
-        			<div>
-	        			<h3 class="mb-3">Premium</h3>
-	        			<p><span class="price">$54.50</span> <span class="per">/ 5mos</span></p>
-	        		</div>
-	        		<div class="img" style="background-image: url(images/bg_3.jpg);"></div>
-        			<div class="px-4">
-	        			<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-        			</div>
-        			<p class="button text-center"><a href="#" class="btn btn-tertiary px-4 py-3">Take A Course</a></p>
-        		</div>
-        	</div>
-        	<div class="col-md-6 col-lg-3 ftco-animate">
-        		<div class="pricing-entry bg-light pb-4 text-center">
-        			<div>
-	        			<h3 class="mb-3">Platinum</h3>
-	        			<p><span class="price">$89.50</span> <span class="per">/ 5mos</span></p>
-	        		</div>
-	        		<div class="img" style="background-image: url(images/bg_5.jpg);"></div>
-        			<div class="px-4">
-	        			<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-        			</div>
-        			<p class="button text-center"><a href="#" class="btn btn-quarternary px-4 py-3">Take A Course</a></p>
-        		</div>
-        	</div>
-        </div>
 
-        <div class="row justify-content-center">
-            <div class="pagination">
-                <a class="btn btn-primary" href="{{ url('gallery') }}">Load More</a>
-             </div>
-
-        </div>
-     </div>
-
-</section>
 @endsection
