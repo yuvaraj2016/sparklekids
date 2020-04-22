2b517b3712e915c23b3b717c8962c8e9dac46296
k2gsalem <k2gsalem@gmail.com>
2020-04-22 18:53:14 +0530

    home testimonial video

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1506,32 +1506,76 @@ text-shadow: 0 0 3px #FF0000;
 
 
             @foreach($testimonials as $testimonial)
-            {{-- @dd($albums) --}}
+            @if($testimonial->type==1)
+
 
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="blog-entry">
-                  <a href="{{ url('testimonial/'.$testimonial->id)}}" class="block-20 d-flex align-items-end" style="background-image: url('http://restschool.hridham.com/storage/testimonials_picture/{{ $testimonial->testimonial_image }}');">
-                                    <div class="meta-date text-center p-2">
-                                    <?php
-                                          $date= explode('-',$testimonial->testimonial_date);
-                                         ?>
-                      <span class="day">{{ $date[0] }}</span>
-                      <span class="mos">{{ date("F", mktime(0, 0, 0, $date[1], 1))}}</span>
-                      <span class="yr">{{ $date[2] }}</span>
-                    </div>
-                  </a>
-                  <div class="text bg-white p-4">
-                    <h3 class="heading">{{ $testimonial->testimonial_title }}</h3>
-                    <p>{{ str_limit(strip_tags($testimonial->testimonial_desc), 120) }}</p>
-                    @if (strlen($testimonial->testimonial_desc)>120)
-                    <div class="d-flex align-items-center mt-4">
-                        <p class="mb-0"><a href="{{ url('testimonial/'.$testimonial->id)}}" class="btn btn-secondary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-                    </div>
-                    @endif
+                    <a href="{{ url('testimonial/'.$testimonial->id) }}"
+                        class="block-20 d-flex align-items-end"
+                        style="background-image: url('http://restschool.hridham.com/storage/testimonials_picture/{{ $testimonial->testimonial_image }}');">
+                        <div class="meta-date text-center p-2">
+                            <?php
+                                      $date= explode('-',$testimonial->testimonial_date);
+                                     ?>
+                            <span class="day">{{ $date[0] }}</span>
+                            <span
+                                class="mos">{{ date("F", mktime(0, 0, 0, $date[1], 1)) }}</span>
+                            <span class="yr">{{ $date[2] }}</span>
+                        </div>
+                    </a>
+                    <div class="text bg-white p-4">
+                        <h3 class="heading">{{ $testimonial->testimonial_name }}</h3>
+                        <p>{{ str_limit(strip_tags($testimonial->testimonial_desc), 120) }}</p>
+                        @if(strlen($testimonial->testimonial_desc)>120)
+                            <div class="d-flex align-items-center mt-4">
+                                <p class="mb-0"><a
+                                        href="{{ url('testimonial/'.$testimonial->id) }}"
+                                        class="btn btn-secondary">Read More <span
+                                            class="ion-ios-arrow-round-forward"></span></a></p>
+                            </div>
+                        @endif
 
-                  </div>
+                    </div>
                 </div>
-              </div>
+            </div>
+        @else
+
+            <div class="col-md-6 col-lg-4 ftco-animate">
+                <div class="blog-entry">
+                    @php
+                        $link = $testimonial->path;
+                        $video_id = explode("?v=", $link); // For videos like http://www.youtube.com/watch?v=...
+                        if (empty($video_id[1]))
+                        $video_id = explode("/v/", $link); // For videos like http://www.youtube.com/watch/v/..
+
+                        $video_id = explode("&", $video_id[1]); // Deleting any other params
+                        $video_id = $video_id[0];
+                    @endphp
+
+
+                    <iframe width="auto" height="aoto"
+                        src={{ 'https://www.youtube.com/embed/'.$video_id }}
+                        frameborder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                    <div class="text bg-white p-4">
+                        <h3 class="heading">{{ $testimonial->testimonial_name }}</h3>
+                        <p>{{ str_limit(strip_tags($testimonial->testimonial_desc), 120) }}</p>
+                        @if(strlen($testimonial->testimonial_desc)>120)
+                            <div class="d-flex align-items-center mt-4">
+                                <p class="mb-0"><a
+                                        href="{{ url('testimonial/'.$testimonial->id) }}"
+                                        class="btn btn-secondary">Read More <span
+                                            class="ion-ios-arrow-round-forward"></span></a></p>
+                            </div>
+                        @endif
+
+                    </div>
+                </div>
+            </div>
+
+        @endif
              @endforeach
 
 
